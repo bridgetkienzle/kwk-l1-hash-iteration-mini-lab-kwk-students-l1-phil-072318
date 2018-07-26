bbfde50b0e555dc3de0ac0f9349ed6dcce74589c
Bridget Kienzle <bridgetkien@gmail.com>
2018-07-26 13:41:01 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,10 +1,47 @@
+# |Place          | Year      |
+# | ------------- |:---------:|
+# | Sydney        | 2000      |
+# | Athens        | 2004      |
+# | Beijing       | 2008      |
+# | London        | 2012      |
 
-def create_olympics_hash
+
+
   # Implement this method so that it returns a hash with the data provided on README.md
-end
+#   places = ["sydney", "athens", "bejing", "London"]
+  
+#   year = [2000, 2004, 2008, 2012]
+ 
+#   olympics_hash = {} 
+#   index = 0 
+  
+#   places.each do |each_place|
+#     olympics_hash[each_place] = year[index]
+#     index += 1 
+# end
 
-def add_a_key_value_pair
+# puts olympics_hash
+
+
+
+
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  #   places = ["sydney", "athens", "bejing", "London"]
+  
+  # year = [2000, 2004, 2008, 2012]
+ 
+  # olympics_hash = {} 
+  # index = 0 
+  
+  # places.each do |each_place|
+  #   olympics_hash[each_place] = year[index]
+  #   index += 1 
+  
+olympics_hash = {"sydney"=>2000, "athens"=>2004, "bejing"=>2008, "London"=>2012}
+  new_place = "brazil"
+  new_year = 2016
+  olympics_hash[new_place] = new_year
+puts olympics_hash
 end
 
 def iterate_through_hash
